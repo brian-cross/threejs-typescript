8b9c340be484efa1d59d66430b83684929421302
Brian Cross <briancross@shaw.ca>
2021-01-04 19:58:24 -0700

    Add extra geometries and gui folders.

--- src/client/client.ts
+++ src/client/client.ts
@@ -5,6 +5,9 @@ import { GUI } from "/jsm/libs/dat.gui.module";
 
 const scene: THREE.Scene = new THREE.Scene();
 
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -19,14 +22,25 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.addEventListener("change", render);
 
-const geometry: THREE.BoxGeometry = new THREE.BoxGeometry();
+const boxGeometry: THREE.BoxGeometry = new THREE.BoxGeometry();
+const sphereGeometry: THREE.SphereGeometry = new THREE.SphereGeometry();
+const icosahedronGeometry: THREE.IcosahedronGeometry = new THREE.IcosahedronGeometry();
+
 const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({
   color: 0x00ff00,
   wireframe: true,
 });
 
-const cube: THREE.Mesh = new THREE.Mesh(geometry, material);
+const cube: THREE.Mesh = new THREE.Mesh(boxGeometry, material);
+cube.position.x = 5;
 scene.add(cube);
+
+const sphere: THREE.Mesh = new THREE.Mesh(sphereGeometry, material);
+sphere.position.x = -5;
+scene.add(sphere);
+
+const icosahedron: THREE.Mesh = new THREE.Mesh(icosahedronGeometry, material);
+scene.add(icosahedron);
 
 camera.position.z = 2;
 
@@ -59,10 +73,124 @@ cubeScaleFolder.add(cube.scale, "x", 0.1, 5, 0.1);
 cubeScaleFolder.add(cube.scale, "y", 0.1, 5, 0.1);
 cubeScaleFolder.add(cube.scale, "z", 0.1, 5, 0.1);
 
-cubeFolder.open();
-cubeRotationFolder.open();
-cubePositionFolder.open();
-cubeScaleFolder.open();
+cubeFolder.add(cube, "visible");
+
+// cubeFolder.open();
+// cubeRotationFolder.open();
+// cubePositionFolder.open();
+// cubeScaleFolder.open();
+
+var cubeData = {
+  width: 1,
+  height: 1,
+  depth: 1,
+  widthSegments: 1,
+  heightSegments: 1,
+  depthSegments: 1,
+};
+const cubePropertiesFolder = cubeFolder.addFolder("Properties");
+cubePropertiesFolder
+  .add(cubeData, "width", 1, 30)
+  .onChange(regenerateBoxGeometry)
+  .onFinishChange(() => console.dir(cube.geometry));
+cubePropertiesFolder
+  .add(cubeData, "height", 1, 30)
+  .onChange(regenerateBoxGeometry);
+cubePropertiesFolder
+  .add(cubeData, "depth", 1, 30)
+  .onChange(regenerateBoxGeometry);
+cubePropertiesFolder
+  .add(cubeData, "widthSegments", 1, 30)
+  .onChange(regenerateBoxGeometry);
+cubePropertiesFolder
+  .add(cubeData, "heightSegments", 1, 30)
+  .onChange(regenerateBoxGeometry);
+cubePropertiesFolder
+  .add(cubeData, "depthSegments", 1, 30)
+  .onChange(regenerateBoxGeometry);
+
+function regenerateBoxGeometry() {
+  let newGeometry = new THREE.BoxGeometry(
+    cubeData.width,
+    cubeData.height,
+    cubeData.depth,
+    cubeData.widthSegments,
+    cubeData.heightSegments,
+    cubeData.depthSegments
+  );
+  cube.geometry.dispose();
+  cube.geometry = newGeometry;
+}
+
+var sphereData = {
+  radius: 1,
+  widthSegments: 8,
+  heightSegments: 6,
+  phiStart: 0,
+  phiLength: Math.PI * 2,
+  thetaStart: 0,
+  thetaLength: Math.PI,
+};
+const sphereFolder = gui.addFolder("Sphere");
+const spherePropertiesFolder = sphereFolder.addFolder("Properties");
+spherePropertiesFolder
+  .add(sphereData, "radius", 0.1, 30)
+  .onChange(regenerateSphereGeometry);
+spherePropertiesFolder
+  .add(sphereData, "widthSegments", 1, 32)
+  .onChange(regenerateSphereGeometry);
+spherePropertiesFolder
+  .add(sphereData, "heightSegments", 1, 16)
+  .onChange(regenerateSphereGeometry);
+spherePropertiesFolder
+  .add(sphereData, "phiStart", 0, Math.PI * 2)
+  .onChange(regenerateSphereGeometry);
+spherePropertiesFolder
+  .add(sphereData, "phiLength", 0, Math.PI * 2)
+  .onChange(regenerateSphereGeometry);
+spherePropertiesFolder
+  .add(sphereData, "thetaStart", 0, Math.PI)
+  .onChange(regenerateSphereGeometry);
+spherePropertiesFolder
+  .add(sphereData, "thetaLength", 0, Math.PI)
+  .onChange(regenerateSphereGeometry);
+
+function regenerateSphereGeometry() {
+  let newGeometry = new THREE.SphereGeometry(
+    sphereData.radius,
+    sphereData.widthSegments,
+    sphereData.heightSegments,
+    sphereData.phiStart,
+    sphereData.phiLength,
+    sphereData.thetaStart,
+    sphereData.thetaLength
+  );
+  sphere.geometry.dispose();
+  sphere.geometry = newGeometry;
+}
+
+var icosahedronData = {
+  radius: 1,
+  detail: 0,
+};
+const icosahedronFolder = gui.addFolder("Icosahedron");
+const icosahedronPropertiesFolder = icosahedronFolder.addFolder("Properties");
+icosahedronPropertiesFolder
+  .add(icosahedronData, "radius", 0.1, 10)
+  .onChange(regenerateIcosahedronGeometry);
+icosahedronPropertiesFolder
+  .add(icosahedronData, "detail", 0, 5)
+  .step(1)
+  .onChange(regenerateIcosahedronGeometry);
+
+function regenerateIcosahedronGeometry() {
+  let newGeometry = new THREE.IcosahedronGeometry(
+    icosahedronData.radius,
+    icosahedronData.detail
+  );
+  icosahedron.geometry.dispose();
+  icosahedron.geometry = newGeometry;
+}
 
 var animate = function () {
   requestAnimationFrame(animate);
