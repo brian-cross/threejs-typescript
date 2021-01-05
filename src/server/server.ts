405df880302d5e77e3b772b11afcce2257490d38
Brian Cross <briancross@shaw.ca>
2021-01-05 00:12:50 -0700

    Formatting change.

--- src/server/server.ts
+++ src/server/server.ts
@@ -18,42 +18,25 @@ class App {
     // Three.js main library
     app.use(
       "/build/three.module.js",
-      express.static(
-        path.join(__dirname, "../../node_modules/three/build/three.module.js")
-      )
+      express.static(path.join(__dirname, "../../node_modules/three/build/three.module.js"))
     );
 
     // Three.js orbit controls
     app.use(
       "/jsm/controls/OrbitControls",
-      express.static(
-        path.join(
-          __dirname,
-          "../../node_modules/three/examples/jsm/controls/OrbitControls.js"
-        )
-      )
+      express.static(path.join(__dirname, "../../node_modules/three/examples/jsm/controls/OrbitControls.js"))
     );
 
     // Three.js FPS display
     app.use(
       "/jsm/libs/stats.module",
-      express.static(
-        path.join(
-          __dirname,
-          "../../node_modules/three/examples/jsm/libs/stats.module.js"
-        )
-      )
+      express.static(path.join(__dirname, "../../node_modules/three/examples/jsm/libs/stats.module.js"))
     );
 
     // Three.js GUI module
     app.use(
       "/jsm/libs/dat.gui.module",
-      express.static(
-        path.join(
-          __dirname,
-          "../../node_modules/three/examples/jsm/libs/dat.gui.module.js"
-        )
-      )
+      express.static(path.join(__dirname, "../../node_modules/three/examples/jsm/libs/dat.gui.module.js"))
     );
 
     this.server = new http.Server(app);
